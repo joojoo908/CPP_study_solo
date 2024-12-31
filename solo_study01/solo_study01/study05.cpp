088a43d07b35a4b986cf84b1da382f3787886f8a
joojoo908 <looloo8910@gmail.com>
2024-12-31 22:07:04 +0900

    함수 포인터
    함수의 주소를 저장하여 동적으로 함수 호출이 가능하도록 해주며 이를 통해 한가지 함수로 여러가지 함수를 사용할 수 있다.
    단, 타입 불일치와 잘못된 함수 호출을 주의하자

---
+++ solo_study01/solo_study01/study05.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// 몇 개의 간단한 함수 정의
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+
+    // 함수 포인터 배열 선언
+    int (*operation[3])(int, int) = { add, subtract, multiply };
+    int (*operation2)(int, int);
+
+    // 배열을 통해 함수 호출
+    cout << "Add: " << operation[0](5, 3) << endl;     
+    cout << "Subtract: " << operation[1](5, 3) << endl;  
+    cout << "Multiply: " << operation[2](5, 3) << endl;   
+
+    operation2 = &add;
+    cout << "Add: " << operation2(5, 3) << endl;
+    operation2 = &subtract;
+    cout << "Subtract: " << operation2(5, 3) << endl;
+    operation2 = &multiply;
+    cout << "Multiply: " << operation2(5, 3) << endl;
+
+}
